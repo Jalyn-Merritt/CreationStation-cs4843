0f6e009a99dfba30d1ae47f0565f561b62cb4880
EC2 Default User <ec2-user@ip-172-31-18-241.ec2.internal>
2022-01-27 04:42:33 +0000

    adding initial structure

---
+++ hello.py
@@ -0,0 +1,6 @@
+def sum2num(x,y):
+    return x+y
+
+x = 1
+y = 2
+print(f"This is the sum: {x}, {y}, {sum2num(x,y)}")
---
+++ test_hello.py
@@ -0,0 +1,5 @@
+from hello import sum2num
+
+def test_sum():
+    assert sum2num(1,2) == 3
+        
